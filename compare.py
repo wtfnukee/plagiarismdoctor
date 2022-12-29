a24d53a16453817b542df1a73c0240cf558ad02b
wtfnukee <hehenukee@gmail.com>
2022-12-29 19:20:45 +0300

    Refactoring and reformatting with Black

--- compare.py
+++ compare.py
@@ -2,16 +2,17 @@ import argparse
 from utils import *
 
 parser = argparse.ArgumentParser(
-    prog='Plagiarism Doctor',
-    description='Yes, it is like Plague Doctor')
-parser.add_argument('input')
-parser.add_argument('output')
+    prog="Plagiarism Doctor", description="Yes, it is like Plague Doctor"
+)
+parser.add_argument("input")
+parser.add_argument("output")
 args = parser.parse_args()
 input_path, output_path = args.input, args.output
 
-with open(input_path, encoding='UTF-8') as inp, \
-        open(output_path, encoding='UTF-8', mode='w') as out:
+with open(input_path, encoding="UTF-8") as inp, open(
+    output_path, encoding="UTF-8", mode="w"
+) as out:
     for i in inp.readlines():
         x, y = i.split()
         score = compare(x, y)
-        out.write(f'{score}\n')
+        out.write(f"{score}\n")
--- utils.py
+++ utils.py
@@ -12,7 +12,11 @@ def levenshtein(a: str, b: str) -> int:
     for i in range(1, m + 1):
         previous_row, current_row = current_row, [i] + [0] * n
         for j in range(1, n + 1):
-            add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+            add, delete, change = (
+                previous_row[j] + 1,
+                current_row[j - 1] + 1,
+                previous_row[j - 1],
+            )
             if a[j - 1] != b[i - 1]:
                 change += 1
             current_row[j] = min(add, delete, change)
@@ -22,39 +26,41 @@ def levenshtein(a: str, b: str) -> int:
 
 def parse_import(i: ast.Import, imports: dict):
     i = i.__dict__
-    names = i['names']
-    modules = [j.__dict__['name'] for j in names]
+    names = i["names"]
+    modules = [j.__dict__["name"] for j in names]
     for module in modules:
         imports[module] = [module]
 
 
 def parse_importfrom(i: ast.ImportFrom, imports: dict):
     i = i.__dict__
-    module = i['module']
-    submodules = [j.__dict__['name'] for j in i['names']]
+    module = i["module"]
+    submodules = [j.__dict__["name"] for j in i["names"]]
     imports[module] = imports.get(module, []) + submodules
 
 
 def parse(x):
     imports = dict()
-    other = ''
+    other = ""
     for i in ast.parse(x).body:
         if isinstance(i, ast.Import):
             parse_import(i, imports)
         elif isinstance(i, ast.ImportFrom):
             parse_importfrom(i, imports)
         else:
-            other += ast.unparse(i) + ' '
+            other += ast.unparse(i) + " "
     return imports, other
 
 
 def compare(x, y):
     imports_x, other_x = parse(x)
     imports_y, other_y = parse(y)
-    return 1 - \
-        dict_size(dict_intersection(imports_x, imports_y)) / (max(dict_size(imports_x), dict_size(imports_y)) + 0.01) \
-        - \
-        levenshtein(other_x, other_y) / max(len(other_x), len(other_y))
+    return (
+        1
+        - dict_size(dict_intersection(imports_x, imports_y))
+        / (max(dict_size(imports_x), dict_size(imports_y)) + 0.01)
+        - levenshtein(other_x, other_y) / max(len(other_x), len(other_y))
+    )
 
 
 def list_intersection(x: list, y: list) -> list:
@@ -63,11 +69,10 @@ def list_intersection(x: list, y: list) -> list:
 
 def dict_intersection(x: dict, y: dict) -> dict:
     return {k: list_intersection(x[k], y[k]) for k in x if k in y}
-    # return {k: x[k] for k in x if k in y and len(x[k]) == len(y[k])}
 
 
 def dict_size(x: dict) -> int:
     total = 0
-    for k, v in x.items():
+    for v in x.values():
         total += len(v)
     return total
